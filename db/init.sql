3ed564d8e23c474652970d66c48380a8a1d39e41
DebugggMax <max.olguin@duocuc.cl>
2026-06-28 22:19:31 -0400

    Cambios clave con las variables de entorno, para el frontendy archivos .yml

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,5 @@
 -- Crear la base de datos si no existe
-CREATE DATABASE IF NOT EXISTS innovatech_db;
+CREATE DATABASE IF NOT EXISTS tienda_db;
 
 -- Seleccionar la base de datos para asegurar que esté lista
-USE innovatech_db;
+USE tienda_db;
